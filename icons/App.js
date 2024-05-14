f62c38e36fa51cdec41a43db444baedbe9cac603
Rennan Alves <rennanavf@gmail.com>
2024-05-14 19:45:54 -0300

    ada

---
+++ icons/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Rennan Alves </Text>
+      <FontAwesome
+      name='home'
+      size={25}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
